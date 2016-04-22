76dec17a7bd9e71ffc696adfeb754afe0b39bc93
Viktor Tsarevskiy <viktor.tsarevskiy@gmail.com>
2016-04-22 23:08:37 +0300

    add support to change maxWidth and gutter on the fly

--- index.js
+++ index.js
@@ -17,13 +17,25 @@ module.exports = postcss.plugin('postcss-grid', function (opts) {
 		var container = cols * columnWidth + (cols -1) * opts.gutter;
 		return ((width  / container) * 100).toFixed(5) * 1;
 	};
+	var gridWidthExtended = function (span, cols, maxWidth, gutter) {
+		var colWidth = (maxWidth - ((cols -1 ) * gutter)) / cols;
+		var width = span * colWidth + (span -1 ) * gutter;
+		var container = cols * colWidth + (cols -1) * gutter;
+		return ((width  / container) * 100).toFixed(5) * 1;
+	};
 
 	var gutterWidth = function (cols) {
 		var width = cols * columnWidth + (cols - 1) * opts.gutter;
 		return ((opts.gutter / width) * 100).toFixed(5) * 1;
 	};
+	var gutterWidthExtended = function (cols, maxWidth, gutter) {
+		var columnWidth = (maxWidth - ((cols -1 ) * gutter)) / cols;
+		var width = cols * columnWidth + (cols - 1) * gutter;
+		return ((gutter / width) * 100).toFixed(5) * 1;
+	};
 
 	var value = /\s*(\d+)\s*\/\s*(\d+)\s*/;
+	var extended = /\s*(\d+)\s*\/\s*(\d+)\s*\/\s*(\d+)\s*\/\s*(\d+)\s*/;
 	var isLast = /\s*!last\s*$/;
 
 	return function (css) {
@@ -51,6 +63,19 @@ module.exports = postcss.plugin('postcss-grid', function (opts) {
 			if (decl.prop === 'grid-column') {
 				var match;
 
+				if (match = extended.exec(decl.value)) {
+					var span = match[1];
+					var columns = match[2];
+					var maxWidth = match[3];
+					var gutter = match[4];
+					decl.parent.append({prop: 'float', value: 'left'}).source = decl.source;
+					decl.parent.append({prop: 'width', value: gridWidthExtended(span, columns, maxWidth, gutter) + '%'}).source = decl.source;
+					if (!(decl.value.match(isLast))) {
+						if (opts.legacy) decl.parent.append({prop: 'display', value: 'inline'}).source = decl.source;
+						decl.parent.append({prop: 'margin-right', value: gutterWidthExtended(columns, maxWidth, gutter) + '%'}).source = decl.source;
+					}
+					return decl.remove();
+				}
 				if (match = value.exec(decl.value)) {
 					var span = match[1];
 					var columns = match[2];
@@ -70,7 +95,18 @@ module.exports = postcss.plugin('postcss-grid', function (opts) {
 			}
 			if (decl.prop === 'grid-push' || decl.prop === 'grid-pull') {
 				var match;
-
+				if (match = extended.exec(decl.value)) {
+					var span = match[1];
+					var columns = match[2];
+					var maxWidth = match[3];
+					var gutter = match[4];
+					var width = span * gridWidthExtended(1, columns, maxWidth, gutter) + span * gutterWidthExtended(columns, maxWidth, gutter);
+					decl.parent.append({
+						prop: decl.prop === 'grid-push' ? 'margin-left' : 'margin-right',
+						value: width + '%'
+					}).source = decl.source;
+					return decl.remove();
+				}
 				if (match = value.exec(decl.value)) {
 					var span = match[1];
 					var columns = match[2];
